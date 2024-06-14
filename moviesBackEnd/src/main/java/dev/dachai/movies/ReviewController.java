de3a5221548a8424f8aa826c1d06214571218865
Nguyen Dac Hai <nguyendmim@gmail.com>
2024-06-14 22:36:28 +0700

    Upload Backend and FrontEnd code of this app, Complete the basic following video youtube in freecodecap

---
+++ moviesBackEnd/src/main/java/dev/dachai/movies/ReviewController.java
@@ -0,0 +1,26 @@
+package dev.dachai.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*")
+public class ReviewController {
+    private final ReviewService reviewService;
+    @Autowired
+    public  ReviewController(ReviewService reviewService){
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload){
+        return new ResponseEntity<Review>( reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+}
